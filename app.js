61c1e3e8e01372b64657a30c26b7736d90750656
ahmedanis45 <ahmedanis4546@gmail.com>
2024-02-26 21:05:37 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var firstName = "Muhammad Ahmed";
+var contact = 9232892892;
+var email = "ahmed@gmail.com";
+var education = "FSC from Abc college - 2021-23  ||   Bachelors in BSCS from UBIT - 2024-28 ";
+var skils= "Web & App  Developer || Mern Developer || <b> UI </b> and <b> UX </b> Desingner";
+var intern = "Contour Software House || Gentect Software House";
+var experience = "<b> 5+ </b> Years in different Softwrare House <br> XEVEN Sol || TECH Hunt";
+var about = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure facere inventore nam eveniet aspernatur animi suscipit ratione a est deserunt? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic quidem, dolor ex cum illum accusantium laborum ipsa aut, laudantium quis soluta saepe, doloribus velit sunt tempore? Incidunt quos provident veritatis alias consequuntur aliquam natus, amet veniam dolores. Est, debitis non. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptate accusantium debitis perferendis optio corporis. Rerum quaerat quae labore amet adipisci aperiam, velit quis, nemo fuga expedita nihil omnis cum."
+var img= "<img style = 'text-align: center' src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAXPQ4zKTX0LFVmTeldb5xNWxvGWBoB-c_dQ&usqp=CAU' alt = ''>"
+
+document.write(
+    img
+)
+
+document.write(
+    "<table border='1px' width = '1000px'> <tr><td><b> Name: </b>"+ firstName + " </td></tr>" + "<br/> <tr><td><b> Conatct No: </b>" + contact +  "</td></tr>" + "<br> <tr><td><b> Email: </b>" + email + "</td><tr>"  + "<br> <tr><td><b> Education: </b>" + education + "</td></td>" + "<tr><td><b> Skills: </b>" + skils + "<tr><td><b> Intern: </b>"+ intern + "</td></tr>" +"</tr></td>" +
+    "<tr><td><b> Experience: </b>" + experience + "</tr></td>" + "<tr><td><b>About:  </b>" + about + "</td></tr>"   + "<table>"
+)
+
+
